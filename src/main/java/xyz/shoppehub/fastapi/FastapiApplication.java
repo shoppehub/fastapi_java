8d22906fd0425bdf6cab774f708162e1389bdd4d
jiaoge <yingjunjiao@outlook.com>
2021-08-20 23:16:31 +0800

    init

---
+++ src/main/java/xyz/shoppehub/fastapi/FastapiApplication.java
@@ -0,0 +1,13 @@
+package xyz.shoppehub.fastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/shoppehub/fastapi/FastapiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.shoppehub.fastapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
